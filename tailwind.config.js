f2d130abd881ca95d99c002cab2545d4fa0ba463
danielgraviet <dgravs5@gmail.com>
2024-11-21 21:28:03 -0700

    edited bio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { default: daisyui } = require('daisyui');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui'),],
+  daisyui: {
+    themes: ['night'],
+  }
+}
+
